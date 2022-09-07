18a7dfee9f3bec2e437cfb1dec10074c594fc034
jimmykodes <jkeith@kochava.com>
2022-09-07 15:09:39 -0700

    feat: init s3 buckets in localstack

--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.18
 require (
 	cloud.google.com/go/pubsub v1.3.1
 	github.com/aws/aws-sdk-go v1.44.56
+	github.com/googleapis/gax-go/v2 v2.4.0
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.12.0
 	go.uber.org/multierr v1.6.0
+	google.golang.org/grpc v1.46.2
 	gopkg.in/yaml.v3 v3.0.0
 )
 
@@ -21,7 +23,6 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
-	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
@@ -43,7 +44,6 @@ require (
 	google.golang.org/api v0.81.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220519153652-3a47de7e79bd // indirect
-	google.golang.org/grpc v1.46.2 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- internal/aws/runner.go
+++ internal/aws/runner.go
@@ -2,13 +2,16 @@ package aws
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/sqs"
 
 	"github.com/jimmykodes/cloud-emulator-init/internal/atomicerr"
@@ -18,6 +21,7 @@ type Config struct {
 	EmulatorURL string   `yaml:"emulatorUrl"`
 	Region      string   `yaml:"region"`
 	SqsQueues   []string `yaml:"sqs"`
+	S3Buckets   []string `yaml:"s3"`
 }
 
 func RunE(ctx context.Context, config *Config) error {
@@ -32,21 +36,23 @@ func RunE(ctx context.Context, config *Config) error {
 		return fmt.Errorf("missing aws region")
 	}
 	sess, err := session.NewSession(&aws.Config{
-		Credentials: credentials.NewStaticCredentials("test", "test", "test"),
-		Region:      &config.Region,
-		Endpoint:    &config.EmulatorURL,
+		Credentials:      credentials.NewStaticCredentials("test", "test", "test"),
+		S3ForcePathStyle: aws.Bool(true),
+		Region:           &config.Region,
+		Endpoint:         &config.EmulatorURL,
 	})
 	if err != nil {
 		return err
 	}
 
+	var (
+		atomicErr atomicerr.Error
+		wg        sync.WaitGroup
+	)
+
 	log.Println("creating aws resources")
 	if len(config.SqsQueues) > 0 {
 		client := sqs.New(sess)
-		var (
-			atomicErr atomicerr.Error
-			wg        sync.WaitGroup
-		)
 
 		for _, queue := range config.SqsQueues {
 			wg.Add(1)
@@ -61,11 +67,34 @@ func RunE(ctx context.Context, config *Config) error {
 				log.Println("created queue:", resp.String())
 			}(&wg, queue)
 		}
-		wg.Wait()
-		if err := atomicErr.Err(); err != nil {
-			return err
+	}
+	if len(config.S3Buckets) > 0 {
+		client := s3.New(sess)
+		for _, bucket := range config.S3Buckets {
+			wg.Add(1)
+			go func(wg *sync.WaitGroup, bucket string) {
+				defer wg.Done()
+				resp, err := client.CreateBucketWithContext(ctx, &s3.CreateBucketInput{Bucket: &bucket})
+				var awsErr awserr.Error
+				if errors.As(err, &awsErr) {
+					if awsErr.Code() != "BucketAlreadyOwnedByYou" {
+						log.Println("error creating bucket", err)
+						atomicErr.Append(err)
+					}
+				} else if err != nil {
+					log.Println("error creating bucket", err)
+					atomicErr.Append(err)
+					return
+				}
+				log.Println("created bucket:", resp.String())
+			}(&wg, bucket)
 		}
 	}
+	wg.Wait()
+	if err := atomicErr.Err(); err != nil {
+		return err
+	}
+
 	log.Println("finished creating aws resources")
 	return nil
 }
